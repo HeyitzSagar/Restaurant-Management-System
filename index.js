8394fd85863ad13762ac1f87ed6d018ca7126838
Sagar Gupta <sagargupta.gola@gmail.com>
2024-05-06 16:08:18 +0530

    Db setup done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const connectDB = require('../server/DbCon/db');
+const port = 3000;
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
